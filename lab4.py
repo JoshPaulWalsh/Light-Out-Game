80a64006fb50098c8176f1324bcaee4b804b7058
Joshua Walsh <joshwalsh12@icloud.com>
2020-10-11 16:47:04 -0700

    Created a random box color

--- lab4.py
+++ lab4.py
@@ -7,16 +7,16 @@ square = [
         [u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1"],
         [u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1"],
 ]
-coloredBox=random.choice(['white','black'])
-if coloredBox=='black':
-    print(u"\u25A1")
-if coloredBox=='white':
-    print(u"\u25A0")
+for i in range(5):
+    coloredBox=random.choice(['white','black'])
+    if coloredBox=='black':
+        square[i]=u"\u25A1"
+    elif coloredBox=='white':
+        square[i]=u"\u25A0"
 
-#for i in range(5):
-#    print(square[i])
-#for i in range(5):
-#    print(u"\u25A1", end=" ")
+for i in range(5):
+    print(square[i], end=" ")
+
 #for i in range(5):
 #    print(u"\u25A1", end=" ")
 #for i in square:
