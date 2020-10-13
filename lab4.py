cece517c9dc4817e38e33890403af3dab8eca72e
Joshua Walsh <joshwalsh12@icloud.com>
2020-10-13 14:51:24 -0700

    Created the grid of squares

--- lab4.py
+++ lab4.py
@@ -1,24 +1,33 @@
 import random
 
-square = [
-        [u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1"],
-        [u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1"],
-        [u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1"],
-        [u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1"],
-        [u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1",u"\u25A1"],
+square =[
+        ["x","x","x","x","x"],
+        ["x","x","x","x","x"],
+        ["x","x","x","x","x"],
+        ["x","x","x","x","x"],
+        ["x","x","x","x","x"],
 ]
-for i in range(5):
-    coloredBox=random.choice(['white','black'])
-    if coloredBox=='black':
-        square[i]=u"\u25A1"
-    elif coloredBox=='white':
-        square[i]=u"\u25A0"
 
 for i in range(5):
-    print(square[i], end=" ")
+    for r in range(5):
+        coloredBox=random.choice(['white','black'])
+        if coloredBox=='black':
+            square[i][r]=u"\u25A1"
+        elif coloredBox=='white':
+            square[i][r]=u"\u25A0"
+
+for i in range(5):
+    for r in range(5):
+        print(square[i][r], end=" ")
+    print("\n")
 
 #for i in range(5):
 #    print(u"\u25A1", end=" ")
 #for i in square:
 #    Square = [[print(u"\u25A1")]*5 for i in range(5)]
 #25A0 is white square
+
+#for i in square:
+#    for r in i:
+#        print(r, end=" ")
+#    print("\n")
